8e32ef9fd27b5fddcdae42e4c5581bfc22fbb4f5
MyNameIsTrez <welfje@gmail.com>
2021-12-23 00:52:50 +0100

    Add and remove images

--- website/src/components/ItemBoxComponent.vue
+++ website/src/components/ItemBoxComponent.vue
@@ -5,37 +5,59 @@
     <div class="sorting-buttons"></div>
     <div class="crafting-status-button"></div>
     <ul class="storage">
-      <li class="item-box" v-for="(item, index) in items" :key="index">
+      <!-- <li class="item-box" v-for="data in $options.item_names" :key="data"> -->
+      <!-- <img
+          class="item"
+          v-lazy="require(`../assets/placeholder_item.png`)"
+          v-bind:alt="item.img"
+        />
+        <div class="count">
+          <div class="count-shadow">150K</div>
+          <div class="count-regular">150K</div>
+        </div> -->
+      <!-- {{ console.log(data) }} -->
+      <!-- </li> -->
+
+      <!-- <li class="item-box" v-for="(item, index) in items" :key="index">
         <img class="item" v-lazy="getImgUrl(item.img)" v-bind:alt="item.img" />
         <div class="count">
           <div class="count-shadow">150K</div>
           <div class="count-regular">150K</div>
         </div>
-      </li>
+        {{ debug(item) }}
+      </li> -->
     </ul>
   </div>
 </template>
 
 <script>
+import ITEM_NAMES_JSON from "../assets/item_names.json";
+// import PLACEHOLDER_ITEM_PNG from "../assets/placeholder_item.png";
 export default {
   data() {
     return {
-      items: []
+      items: [],
+      item_names: ITEM_NAMES_JSON,
+      //   placeholder_item_png: PLACEHOLDER_ITEM_PNG,
     };
   },
   mounted() {
     const renders = require.context("@/assets/renders", true, /^.*\.png$/);
-    renders.keys().map(key => {
+    renders.keys().map((key) => {
       // './OpenBlocks/elevator@3.png' -> '/OpenBlocks/elevator@3.png'
       this.items.push({ img: key.replace(".", "") });
     });
+    console.log(this.item_names);
     // console.log(`Item count: ${this.items.length}`);
   },
   methods: {
     getImgUrl(pic) {
       return require(`../assets/renders${pic}`);
-    }
-  }
+    },
+    debug(msg) {
+      console.log(msg);
+    },
+  },
 };
 </script>
 
@@ -51,11 +73,13 @@ export default {
   display: flex;
   position: relative;
   background-image: url("../assets/item-box.svg");
+  user-select: none;
 }
 
 .item {
   padding: 10px;
   width: 150px;
+  image-rendering: pixelated;
 }
 
 @font-face {
